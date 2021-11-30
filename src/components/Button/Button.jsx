459491d2eae4b69b922c0a42b746a8e5e7174d1e
Matej Mok <matej.mok@gmail.com>
2021-11-30 15:19:56 +0100

    test library files

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Button() {
+  return (<button>Library button</button>);
+};
+
+export default Button;
+
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname),
+        filename: "index.js",
+        libraryTarget: "umd"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+}
